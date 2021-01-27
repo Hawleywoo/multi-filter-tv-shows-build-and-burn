065d4dcac3a167f0a974fc52431245dc2685dc3e
LimitToInfinity <21stLimitless@gmail.com>
2021-01-27 10:58:01 -0700

    Fetches tv shows and sets state.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import { Component } from 'react';
 
 import './App.css';
 
+const tvShowsURL = 'https://api.tvmaze.com/shows';
+
 class App extends Component {
+
+  state = {
+    tvShows: []
+  }
+
+  componentDidMount() {
+    fetch(tvShowsURL)
+      .then(response => response.json())
+      .then(tvShows => this.setState({ tvShows }));
+  }
   
   render() {
     return (
